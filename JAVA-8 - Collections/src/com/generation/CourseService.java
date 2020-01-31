c570fa64d7bffee9a7a5a303970ee811bf9e8861
sancarbar <sancarbar@gmail.com>
2020-01-31 13:18:28 -0500

    Added source code for JAVA-8

---
+++ JAVA-8 - Collections/src/com/generation/CourseService.java
@@ -0,0 +1,45 @@
+package com.generation;
+
+import java.util.HashMap;
+
+public class CourseService
+{
+    HashMap<String, Student> students = new HashMap<>();
+
+    HashMap<String, Course> courses = new HashMap<>();
+
+
+    public CourseService()
+    {
+        students.put( "120120", new Student( "Santiago", "120120" ) );
+        students.put( "884545", new Student( "Kate", "884545" ) );
+        students.put( "458787", new Student( "Alejandra", "458787" ) );
+        students.put( "135464", new Student( "Maria", "135464" ) );
+        students.put( "778979", new Student( "Peter", "778979" ) );
+
+        courses.put( "math_01", new Course( "Mathematics 1", "math_01", 8 ) );
+        courses.put( "biol_01", new Course( "Biology 1", "biol_01", 8 ) );
+        courses.put( "phys_01", new Course( "Physics 1", "phys_01", 8 ) );
+        courses.put( "art_01", new Course( "Arts 1", "art_01", 8 ) );
+        courses.put( "chem_01", new Course( "Chemistry 1", "chem_01", 8 ) );
+        courses.put( "sport_01", new Course( "Sports 1", "sport_01", 8 ) );
+    }
+
+    public void enrollStudent(String studentId, String courseId){
+        //TODO implement so it adds the given course form the student
+    }
+
+    public void unEnrollStudent(String studentId, String courseId){
+        //TODO implement so it removes the given course form the student
+    }
+
+    public void displayCourseInformation(String courseId){
+        //TODO implement so it shows the course name, id and credits
+    }
+
+    public void displayStudentInformation(String studentId){
+        //TODO implement so it shows the student name, id and list of enrolled courses
+    }
+
+
+}
